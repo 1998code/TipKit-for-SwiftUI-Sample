16c0c746881481c2cd4c2d4dd4bc0408c57956c6
1998code <54872601+1998code@users.noreply.github.com>
2023-07-26 20:45:56 +0800

    Initial

---
+++ TipKit Sample/ContentView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  TipKit Sample
+//
+//  Created by Ming on 26/7/2023.
+//
+
+import SwiftUI
+import TipKit
+
+struct ContentView: View {
+    var bookmarkTip = BookmarkTip()
+    var body: some View {
+        VStack {
+            TipView(bookmarkTip, arrowEdge: .bottom)
+            Button(action: {}) {
+                Label("Bookmark", systemImage: "star")
+            }
+        }
+        .padding()
+        .task {
+            // Configure and load your tips at app launch.
+            try? await Tips.configure {
+                DisplayFrequency(.immediate)
+                DatastoreLocation(.applicationDefault)
+            }
+        }
+    }
+}
+
+// MARK: - Define tip's content here
+struct BookmarkTip: Tip {
+    // Set Image Icon
+    var asset: Image? {
+        Image(systemName: "star")
+    }
+    // Set Title
+    var title: Text {
+        Text("Click and Bookmark")
+    }
+    // Set Message to explain details
+    var message: Text? {
+        Text("Save this page and return here anytime.")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TipKit Sample/TipKit_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipKit_SampleApp.swift
+//  TipKit Sample
+//
+//  Created by Ming on 26/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct TipKit_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
